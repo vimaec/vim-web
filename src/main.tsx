a406ce668a65d7993159224f1698bea2ea066660
vim-sroberge <simon.roberge@vimaec.com>
2024-10-25 12:29:38 -0400

    main update

--- src/main.tsx
+++ src/main.tsx
@@ -16,32 +16,24 @@ demo()
 async function demo () {
   const cmp = await createVimComponent(undefined, getLocalSettings())
 
-  const request = await cmp.loader.request(
-    {
-      url: 'https://saas-api-dev.vimaec.com/api/public/8A12977A-E69B-42DC-D05B-08DCE88D23C7/2024.10.11',
-      headers: {
-        Authorization: 'yJSkyCvwpksvnajChA64ofKQS2KnB24ADHENUYKYTZFZc4SzcWa5WPwJNzTvrsZ8sv8SL8R69c92TUThFkLi1YsvpGxnZFExWs5mbQisuWyhBPAXosSEUhPXyUaXHHBJ'
-      }
-    },
-    {
-      progressive: true,
-      rotation: new THREE.Vector3(270, 0, 0)
-    }
-  )
+  const request = await cmp.loader.request({
+    url: url ?? 'https://vimdevelopment01storage.blob.core.windows.net/samples/Wolford_Residence.r2025.vim'
+  }, {
+    rotation: new THREE.Vector3(270, 0, 0)
+  })
 
   for await (const progress of request.getProgress()) {
     console.log(`Downloading Vim (${(progress.loaded / 1000).toFixed(0)} kb)`)
   }
-
   const result = await request.getResult()
   if (result.isError()) {
-    console.error('Error loading vim', result.error)
+    console.error(result.error)
     return
   }
-
-  cmp.loader.add(result.result)
+  const vim = result.result
+  cmp.loader.add(vim)
 
   globalThis.THREE = THREE
   globalThis.component = cmp
-  globalThis.vim = result.result
+  globalThis.vim = vim
 }
