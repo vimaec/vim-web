200a675066a80100dc6d49265d3c08ed71062b1c
vim-sroberge <simon.roberge@vimaec.com>
2024-12-04 16:42:55 -0500

    updated linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,36 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['standard'],
-  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: './tsconfig.json' // Include if using type-aware linting
   },
-  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {  
+      version: 'detect'
+    }
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking', // Include if using type-aware linting
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier' // Include if integrating Prettier
+  ],
   rules: {
-    'no-use-before-define': 'off'
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    // Add any additional custom rules here
   },
   globals: {
     JSX: true
-  }
+  },
+  ignorePatterns: ['postcss.config.js', '.eslintrc.js']
 }
