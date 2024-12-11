e7657b9305c88a762690ca74c672b2cee375d8fd
vim-sroberge <simon.roberge@vimaec.com>
2024-12-11 12:40:48 -0500

    fixed export

--- src/vim-web/index.ts
+++ src/vim-web/index.ts
@@ -1,5 +1,6 @@
 export * as WebglViewer from './core-viewers/webgl'
-export * as WebglReact from './react-viewers/webgl'
 export * as UltraViewer from './core-viewers/ultra'
-export * as UltraReact from './react-viewers/ultra'
+export * from './react-viewers'
 export * as THREE from 'three'
+
+
