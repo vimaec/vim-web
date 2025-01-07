e2d12669706280cbaa8b85e7ba6ee4a6f96977d0
vim-sroberge <simon.roberge@vimaec.com>
2025-01-07 12:20:08 -0500

    update cdn urls

--- src/pages/devUrls.ts
+++ src/pages/devUrls.ts
@@ -1,8 +1,8 @@
-export const residence = 'https://vim.azureedge.net/samples/residence.v1.2.75.vim'
+export const residence = 'https://storage.cdn.vimaec.com/samples/residence.v1.2.75.vim'
 
 // Ask Martin or Simon for the access token
 export const residenceWithAccessToken = 'https://saas-api-v2.vimaec.com/api/public/542c2335-992d-4af0-ffd9-08dd0262dd9c/2024-11-11T15:09:43'
 
-export const notAVim = 'https://vimdevelopment01storage.blob.core.windows.net/samples/not_a_vim.vim'
+export const notAVim = 'https://storage.cdn.vimaec.com/samples/not_a_vim.vim'
 
-export const medicalTower = 'https://vim.azureedge.net/samples/Medical_Tower.vim'
+export const medicalTower = 'https://storage.cdn.vimaec.com/samples/Medical_Tower.vim'
