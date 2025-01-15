0926291baddbe42238fb3eb739c9a3c85888f337
vim-sroberge <simon.roberge@vimaec.com>
2025-01-15 14:13:52 -0500

    updated pages

--- src/pages/ultra/ultraPageIndex.tsx
+++ src/pages/ultra/ultraPageIndex.tsx
@@ -12,11 +12,13 @@ import { UltraBackground } from './09_background'
 import { UltraIblLock } from './10_iblLock'
 import { UltraGhostColor } from './11_ghostColor'
 import { UltraResize } from './12_resize'
+import { UltraAccessToken } from './00a_accessToken'
 
 export const gitRoot = 'https://github.com/vimaec/vim-web/tree/main/src/pages/ultra'
 
 export const ultraPages = [
   { path: '/ultra', page: gitRoot+'/00_home.tsx', component: <UltraHome /> },
+  { path: '/ultra/access_token', page: gitRoot+'/00a_accessToken.tsx', component: <UltraAccessToken /> },
   { path: '/ultra/connection_error', page: gitRoot+'/01_connectionError.tsx', component: <UltraConnectionError /> },
   { path: '/ultra/download_error', page: gitRoot+'/02_downloadError.tsx', component: <UltraDownloadError /> },
   { path: '/ultra/open_error', page: gitRoot+'/03_openError.tsx', component: <UltraOpenError /> },
