cfcffa1506212e73b6580ff089c87caa6bc3944e
vim-sroberge <simon.roberge@vimaec.com>
2025-05-01 15:18:40 -0400

    removed pages

--- src/pages/ultra/testUtils.ts
+++
@@ -1,7 +0,0 @@
-export function generateRandomIndices (count: number, maxValue: number): number[] {
-  const randomIndices = new Set<number>()
-  while (randomIndices.size < count) {
-    randomIndices.add(Math.floor(Math.random() * maxValue))
-  }
-  return Array.from(randomIndices)
-}
