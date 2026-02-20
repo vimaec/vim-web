344655b051abf4cc4ad03f12f4b48ca08fe03ac0
vim-sroberge <simon.roberge@vimaec.com>
2026-02-20 12:31:44 -0500

    standard icons

--- src/vim-web/react-viewers/controlbar/controlBarButton.tsx
+++ src/vim-web/react-viewers/controlbar/controlBarButton.tsx
@@ -1,11 +1,12 @@
 import * as Style from './style'
+import { IconOptions } from '../icons'
 
 export interface IControlBarButton {
   id: string,
   enabled?: (() => boolean) | undefined
   tip: string
   action: () => void
-  icon: ({ height, width, fill, className }) => JSX.Element
+  icon: (options?: IconOptions) => JSX.Element
   isOn?: () => boolean
   style?: (on: boolean) => string
 }
@@ -16,7 +17,7 @@ export function createButton (button: IControlBarButton) {
 
   return (
     <button key={button.id} id={button.id} data-tip={button.tip} onClick={button.action} className={style} type="button">
-      {button.icon({ height: '20', width: '20', fill: 'currentColor', className: 'vc-max-h-[80%]' })}
+      {button.icon({ className: 'vc-max-h-[80%]' })}
     </button>
   )
 }
